b6b3e78a1ca5400b538a9045f1806437e30f00f9
Igor Veljovic <196611749+AIV-git@users.noreply.github.com>
2025-03-05 19:42:52 +1100

    Create function to emulate a single round
    Add logic to a new function to play a single round and increment scores for human/computer

--- script.js
+++ script.js
@@ -3,3 +3,41 @@ function getComputerChoice() {
     const random = Math.floor(Math.random() * values.length);
     return values[random]
 }
+
+function getHumanChoice() {
+    let input = prompt("What will you choose?");
+    //console.log(input);
+    return input
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice.toLowerCase() == computerChoice.toLowerCase()) {
+        console.log("It's a tie!")
+    }
+    else if ((humanChoice.toLowerCase() == 'rock') && (computerChoice.toLowerCase() == 'paper') || 
+    (humanChoice.toLowerCase() == 'paper') && (computerChoice.toLowerCase() == 'scissors') ||
+    (humanChoice.toLowerCase() == 'scissors') && (computerChoice.toLowerCase() == 'rock')) {
+        computerScore++;
+    }
+    else if ((humanChoice.toLowerCase() == 'rock') && (computerChoice.toLowerCase() == 'scissors') || 
+    (humanChoice.toLowerCase() == 'paper') && (computerChoice.toLowerCase() == 'rock') ||
+    (humanChoice.toLowerCase() == 'scissors') && (computerChoice.toLowerCase() == 'paper')) {
+        humanScore++;
+    }
+    
+    if (humanScore > computerScore) {
+        return console.log("Human wins with score: " + humanScore);
+    } else if (computerScore > humanScore) {
+        return console.log("Computer wins with score: " + computerScore);
+    } else {
+        return console.log("The human and computer ended in a tie!");
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
