e2398469a6da20b25faddb8ae32662c8252d3cba
Igor Veljovic <196611749+AIV-git@users.noreply.github.com>
2025-03-12 11:09:22 +1100

    Updated javascript
    Final iteration of Javascript to output results into div elements

--- script.js
+++ script.js
@@ -4,19 +4,25 @@ function getComputerChoice() {
     return values[random];
 }
 
-const computerSelection = getComputerChoice();
-
-let humanScore = 0;
-let computerScore = 0;
+let humanScore = 1;
+let computerScore = 1;
 
 const body = document.querySelector("body");
 const roundOutput = document.createElement("div");
 const resultOutput = document.createElement("div");
 
+const playerScoreboard = document.createElement("div");
+const computerScoreboard =  document.createElement("div");
+playerScoreboard.innerHTML = `<legend>Player Score</legend>`;
+computerScoreboard.innerHTML = `<legend>Computer Score</legend>`;
+
 body.appendChild(roundOutput);
 body.appendChild(resultOutput);
+body.appendChild(playerScoreboard);
+body.appendChild(computerScoreboard);
 
-function playRound(humanChoice, computerChoice) {
+function playRound(humanChoice) {
+    let computerChoice = getComputerChoice();
     if (humanChoice == computerChoice) {
         roundOutput.textContent = "It's a tie! You chose: " + humanChoice + ". The computer chose: " + computerChoice
     }
@@ -24,37 +30,29 @@ function playRound(humanChoice, computerChoice) {
     (humanChoice == 'paper' && computerChoice == 'scissors') ||
     (humanChoice == 'scissors' && computerChoice == 'rock')) {
         roundOutput.textContent = "The computer won! your choice: " + humanChoice + ". The computer chose: " + computerChoice;
-        computerScore++;
+        computerScoreboard.innerHTML = "Computer Score: " + computerScore++;
     }
     else if ((humanChoice == 'rock' && computerChoice == 'scissors') || 
     (humanChoice == 'paper' && computerChoice == 'rock') ||
     (humanChoice == 'scissors' && computerChoice == 'paper')) {
         roundOutput.textContent = "You won! your choice: " + humanChoice + ". The computer chose: " + computerChoice;
-        humanScore++;
+        playerScoreboard.innerHTML = "Player Score: " + humanScore++;
     }
 }
 
 const buttons = document.querySelectorAll("button");
 let gameScore = 0;
 
-const playerScoreboard = document.createElement("div");
-const computerScoreboard =  document.createElement("div");
-playerScoreboard.style.display = "inline";
-playerScoreboard.innerHTML = `<legend>Player</legend>`;
-computerScoreboard.style.display = "inline";
-computerScoreboard.innerHTML = `<legend>Computer</legend>`;
-
-body.appendChild(playerScoreboard);
-body.appendChild(computerScoreboard);
-
 buttons.forEach((button) => {
     button.addEventListener("click", (event) => {
         let humanSelection = button.value;
+        playRound(humanSelection);
+
         if (gameScore >= 5) {
-            console.log('End of 5 round game.');
+            resultOutput.textContent = 'End of 5 round game.';
+            return;
         } else {
             gameScore++;
         }
-        playRound(humanSelection, computerSelection);
     });
 });
